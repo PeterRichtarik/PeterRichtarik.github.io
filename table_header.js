aac76e4a707a9e8c0f8de4ff8e41134dfbee7fc8
Peter Richtarik <peter.richtarik@ed.ac.uk>
2019-04-13 21:44:19 +0300

    orcid

--- table_header.js
+++ table_header.js
@@ -13,8 +13,14 @@ document.write("E-mail: peter.richtarik@kaust.edu.sa<br>");
 document.write("<br>");
 document.write("&nbsp;&nbsp;&nbsp;&nbsp; ");
 document.write("<a href=\"https:\/\/scholar.google.com\/citations?user=pGh242UAAAAJ&amp;hl=en\"><img src=\"imgs\/scholar.png\" alt=\"Google Scholar\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
+document.write("<a href=\"https:\/\/orcid.org\/0000-0003-4380-5848\"> <img src=\"imgs\/orcid.png\" alt=\"ORCID\" width=\"25\" border=\"0\" height=\"25\"><\/a> &nbsp;");
 document.write("<a href=\"https:\/\/www.linkedin.com\/in\/richtarik\/\"> <img src=\"imgs\/linkedin.png\" alt=\"Linked In\" width=\"25\" border=\"0\" height=\"25\"><\/a><br>");
 document.write("<\/td>");
 document.write("<\/tr>");
 document.write("<\/tbody>");
 document.write("<\/table>");
+
+
+
+
+
