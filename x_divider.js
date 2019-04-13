1342e3293ee778b1eda02e8d814e1c54f8957474
Peter Richtarik <peter.richtarik@ed.ac.uk>
2019-04-13 18:22:47 +0300

    first commit

---
+++ x_divider.js
@@ -0,0 +1,5 @@
+document.write("<br>");
+document.write("<br>");
+document.write("<img alt=\"\" src=\"imgs\/fancy-line.png\" width=\"196\" height=\"20\">");
+document.write("<br>");
+document.write("<br>");
