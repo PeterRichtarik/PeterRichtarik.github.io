4c5f6021ce16bdcd653a3a6b6379abb1942d0d90
Peter Richtarik <richtarik@gmail.com>
2021-06-10 09:31:30 +0200

    xx

--- table_header.js
+++ table_header.js
@@ -1,4 +1,4 @@
-document.write("<table width=\"805\" border=\"0\" height=\"184\">");
+document.write("<table width=\"815\" border=\"0\" height=\"184\">");
 document.write("<tbody>");
 document.write("<tr>");
 document.write("<td> <img src=\"imgs\/pr.jpg\" alt=\"Peter Richtarik\"");
