9f719d4813c4252942a1a51835f342d24b28f905
Peter Richtarik <richtarik@gmail.com>
2024-09-08 13:53:20 +0300

    updates

--- x_footer.js
+++ x_footer.js
@@ -4,8 +4,6 @@ document.write("<br>");
 document.write("<br>");
 document.write("<br>");
 document.write(" — <a href=\"https:\/\/ml.kaust.edu.sa\">The ML Hub<\/a>");
-document.write(" — <a href=\"https:\/\/vcc.kaust.edu.sa\">VCC<\/a>");
-document.write(" — <a href=\"https:\/\/ecrc.kaust.edu.sa\">ECRC<\/a>");
 document.write(" — <a href=\"https:\/\/cemse.kaust.edu.sa\/\">CEMSE<\/a>");
 document.write(" — <a href=\"https:\/\/cemse.kaust.edu.sa\/amcs\">AMCS<\/a>");
 document.write(" — <a href=\"https:\/\/cemse.kaust.edu.sa\/cs\">CS<\/a>");
