9f2f6cb5c6700c4d2c2daeca45189fe6647dd2b3
Peter Richtarik <richtarik@gmail.com>
2026-08-25 21:21:34 +0200

    Update bio service and homepage copy; switch pages to shared nav.
    Record current SAC/AC roles, move EPSRC to past appointments, fix consulting office and a few grammar/talks typos, and drop the unused divider script.

--- x_divider.js
+++
@@ -1,5 +0,0 @@
-document.write("<br>");
-document.write("<br>");
-document.write("<img alt=\"\" src=\"imgs\/fancy-line.png\" width=\"196\" height=\"20\">");
-document.write("<br>");
-document.write("<br>");
